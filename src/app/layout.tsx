b53ecdd22b48fb43965c86da53aa62fd7767b189
Karm0s <18452314+Karm0s@users.noreply.github.com>
2023-12-01 22:00:38 -0500

    Initial commit.

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'VimNotes',
+  description: 'Note taking app with Vim editing style',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
